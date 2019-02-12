b8ff66d609e91c504b3cdb7157014d86f4c19f8f
TaliaFontaine <taliafontaine1@gmail.com>
2019-02-12 22:17:46 +0000

    step2 of http request assignment

---
+++ step2.js
@@ -0,0 +1,32 @@
+// https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+};
+
+//https.get takes a callback with one parameter - response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function getAndPrintHTMLChunks(response) {
+// set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    var sections = "";
+// console.log each chunk of data as it is received, concatenated with a newline character ('\n') to visualize each chunk
+    response.on('data', function(data) {
+// sections is the buffer that appends all the data in a string
+    sections += data;
+
+        console.log('Chunk Received. Length:', data.length + '\n');
+
+    });
+// the callback in invoked when a 'data' chunk is received
+
+//the callback is invoked when all of the data has been received
+// (the 'end' of the stream)
+    response.on('end', function() {
+        console.log('Response stream complete.');
+        console.log(sections);
+    });
+
+});
