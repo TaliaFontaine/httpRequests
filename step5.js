983933ea12c2011ce841fc1952da70da5b2a3864
TaliaFontaine <taliafontaine1@gmail.com>
2019-02-13 23:04:44 +0000

    step5 of http request assignment

--- step5.js
+++ step5.js
@@ -4,14 +4,9 @@ function printHTML (html) {
   console.log(html);
 }
 
-function printHTMLUppercase (html){
-    console.log(html.toUpperCase());
-}
-
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
 
 getAndPrintHTML(requestOptions, printHTML);
-getAndPrintHTML(requestOptions, printHTMLUppercase);
