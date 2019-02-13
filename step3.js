4312c93ea68ce8cd464a8e109e8f622f0c7e148b
TaliaFontaine <taliafontaine1@gmail.com>
2019-02-13 20:35:28 +0000

    steps for http request assignment

---
+++ step3.js
@@ -0,0 +1,37 @@
+// https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+function getAndPrintHTML(options) {
+     //https.get takes a callback with one parameter - response, which is a Stream that represents the HTTP response
+    https.get(options, function getHTML(response) {
+    // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+
+        var sections = "";
+    // console.log each chunk of data as it is received, concatenated with a newline character ('\n') to visualize each chunk
+        response.on('data', function(data) {
+    // sections is the buffer that appends all the data in a string
+        sections += data;
+
+            console.log('Chunk Received. Length:', data.length + '\n');
+
+        });
+    // the callback in invoked when a 'data' chunk is received
+
+    //the callback is invoked when all of the data has been received
+    // (the 'end' of the stream)
+        response.on('end', function() {
+            console.log('Response stream complete.');
+            console.log(sections);
+        });
+
+    });
+}
+
+var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+};
+
+
+getAndPrintHTML(requestOptions);
---
+++ step4.js
@@ -0,0 +1,46 @@
+// https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+function getAndPrintHTML(options, callback) {
+     //https.get takes a callback with one parameter - response, which is a Stream that represents the HTTP response
+    https.get(options, function getHTML(response) {
+    // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+
+        var sections = "";
+    // console.log each chunk of data as it is received, concatenated with a newline character ('\n') to visualize each chunk
+        response.on('data', function(data) {
+    // sections is the buffer that appends all the data in a string
+        sections += data;
+
+            console.log('Chunk Received. Length:', data.length + '\n');
+
+        });
+    // the callback in invoked when a 'data' chunk is received
+
+    //the callback is invoked when all of the data has been received
+    // (the 'end' of the stream)
+        response.on('end', function() {
+            console.log('Response stream complete.');
+            callback(sections);
+        });
+
+    });
+}
+//How it is printed
+function printHTML (html) {
+  console.log(html);
+}
+//How html is printed
+function printHTMLUppercase (html){
+    console.log(html.toUpperCase());
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+getAndPrintHTML(requestOptions, printHTML);
+getAndPrintHTML(requestOptions, printHTMLUppercase);
+
+
